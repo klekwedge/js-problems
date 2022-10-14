b7d5b53ab7b5008d4edcbef6c6f3f1ee32bc1c4f
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-14 21:22:53 +0300

    feat: add a solution to task number 2

---
+++ js-problems/problems/002-basketball.js
@@ -0,0 +1,37 @@
+/**
+ * Известны результаты каждой из 4х четвертей баскетбольной встречи.
+ * Нужно определить победителя матча. Побеждает команда,
+ * набравшая больше очков в течение всего матча.
+ *
+ * Напишите функцию getWinner(points) возвращающую номер победившей команды,
+ * либо undefined в случае ничьей.
+ *
+ * Пример:
+
+ * getWinner(['23-26', '24-30', '30-27', '35-31']) === 2
+ * getWinner(['36-32', '32-24', '20-28', '30-26']) === 1
+ * getWinner(['36-18', '22-31', '27-21', '19-34']) === undefined
+ *
+ * @param {string[]} points
+ * @returns {(number|undefined)}
+ */
+function getWinner(points) {
+  let firstTeamPoints = 0;
+  let secondTeamPoints = 0;
+
+  points.forEach((item) => {
+    const res = item.split('-');
+    firstTeamPoints += +res[0];
+    secondTeamPoints += +res[1];
+  });
+
+  if (firstTeamPoints > secondTeamPoints) {
+    return 1;
+  } else if (firstTeamPoints < secondTeamPoints) {
+    return 2;
+  }
+
+  return undefined;
+}
+
+module.exports = getWinner;
