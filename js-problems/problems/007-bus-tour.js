c5e64374e946e4ac0b4e5560b7a3dc09743e9485
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-14 22:01:50 +0300

    feat: add a solution to task number 7

---
+++ js-problems/problems/007-bus-tour.js
@@ -0,0 +1,25 @@
+/**
+ * На экскурсионном маршруте автобусу высотой 512 см встречаются несколько мостов.
+ *
+ * Напишите функцию checkBusTour(bridges) вычисляющую номер моста под которым не сможет проехать автобус.
+ *
+ * Пример:
+ *
+ * checkBusTour([600, 512]) === 2
+ * checkBusTour([600, 1024]) === undefined  # Автобус успешно проедет по маршруту
+ *
+ * @param {number[]} bridges высоты мостов встречающихся на маршруте
+ * @returns {undefined|number}
+ */
+function checkBusTour(bridges) {
+  for (let i = 0; i < bridges.length; i++) {
+    const bridge = bridges[i];
+    if (bridge <= 512) {
+      return i + 1;
+    }
+  }
+
+  return undefined;
+}
+
+module.exports = checkBusTour;
