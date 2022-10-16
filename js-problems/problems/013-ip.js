6835a7365c148fe440ae6962bdcb0d492d417399
klekkwedge <antonov.andrey.10101@gmail.com>
2022-10-16 17:07:58 +0300

    feat: add a solution to task number 13

---
+++ js-problems/problems/013-ip.js
@@ -0,0 +1,38 @@
+/**
+ * Для того чтобы выходить в Интернет, каждому компьютеру присваивается так называемый IP-адрес.
+ * Он состоит из четырех целых чисел в диапазоне от 0 до 255, разделенных точками.
+ * В следующих трех строках показаны три правильных IP-адреса:
+ *  127.0.0.0
+ *  192.168.0.01
+ *  255.000.255.0255
+ *
+ * Напишите функцию isIpValid(address) опредяляющую является ли переданная строка правильным IP-адресом.
+ *
+ * Пример:
+ *
+ * isIpValid('127.0.0.1') === true
+ * isIpValid('127.0.0.0255') === true
+ * isIpValid('127.0.256.0255') === false
+ * isIpValid('Hello world') === false
+ *
+ * @param {string} address
+ * @returns {boolean}
+ */
+function isIpValid(address) {
+  const parts = address.split('.');
+  if (parts.length !== 4) {
+    return false;
+  }
+
+  for (const part of parts) {
+    if (+part === NaN) {
+      return false;
+    } else if (+part > 255 || +part < 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+module.exports = isIpValid;
